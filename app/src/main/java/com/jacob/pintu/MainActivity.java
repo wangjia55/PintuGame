76a57b703706bba4a17cf343dfd82222987284e8
jacob-wj <wangjia199055@126.com>
2015-04-17 15:55:14 +0800

    完成了闯关成功的逻辑判断

--- app/src/main/java/com/jacob/pintu/MainActivity.java
+++ app/src/main/java/com/jacob/pintu/MainActivity.java
@@ -1,17 +1,38 @@
 package com.jacob.pintu;
 
+import android.app.AlertDialog;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends FragmentActivity {
 
+    private PinTuLayout mPintuLayout;
+    private TextView mTextViewLevel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextViewLevel = (TextView) findViewById(R.id.textView_level);
+
+        mPintuLayout = (PinTuLayout) findViewById(R.id.pintuLayout);
+        mPintuLayout.setOnGameListener(new PinTuLayout.OnGameListener() {
+            @Override
+            public void gameStart(int level) {
+                mTextViewLevel.setText("第 " + level + " 关");
+            }
+
+            @Override
+            public void gameSuccess(int level) {
+                Toast.makeText(MainActivity.this,"第"+level+" 关成功！",Toast.LENGTH_SHORT).show();
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).setTitle("拼图游戏").setMessage("闯关成功").create();
+
+            }
+        });
     }
 }
